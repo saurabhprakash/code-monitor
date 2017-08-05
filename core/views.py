9268ab16b3ddc8c40172535a2fcf85a72247bacf
saurabh <saurabh@kuliza.com>
2017-08-06 03:49:40 +0530

    change in viewsets implementation

--- core/views.py
+++ core/views.py
@@ -1,11 +1,12 @@
 from rest_framework.decorators import detail_route
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from core.models import CodeStandardData
 from core.serializers import CodeStandardDataSerializer
 
 
-class MonitorView(viewsets.ModelViewSet):
+class MonitorView(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, 
+            mixins.ListModelMixin, viewsets.GenericViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
